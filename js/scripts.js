ec15bd75c14a3d43f7cce3620aa8d0412ddf1f9a
De Havilland Fox <djfox8705@gmail.com>
2025-03-14 10:54:13 -0400

    add wishlist

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
-function verifyAge() { 
-    const age = document.getElementById('age').value; 
-    if (age >= 18)
-        { document.getElementById('ageVerification').style.display = 'none';
-        document.getElementById('protectedContent').style.display = 'block';
-    } else { alert('You must be at least 18 years old to view this content.'); }
+function verifyAge(isOfAge) {
+      if (isOfAge) {
+        document.getElementById('ageGate').style.display = 'none';
+        document.getElementById('ageVerifiedContent').style.display = 'block';
+      } else {
+        alert('Sorry, you must be 18 or older to access this content.');
+      }
 }
